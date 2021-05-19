02a31813b537bb6499193acd5cf506900c94c32a
Orito Itsuki <20170107+MatchaChoco010@users.noreply.github.com>
2021-05-19 23:26:57 +0900

    first commit

---
+++ KoharuRikka.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using CeVIO.Talk.RemoteService2;
+
+namespace CeVIOAIErrorTest
+{
+    public class KoharuRikka : IDisposable
+    {
+        const string CastName = "小春六花";
+
+        readonly Talker2 _talker;
+
+        public KoharuRikka()
+        {
+            ServiceControl2.StartHost(false);
+            Console.WriteLine(ServiceControl2.HostVersion);
+            _talker = new Talker2(CastName);
+        }
+
+        public void Dispose()
+        {
+            ServiceControl2.CloseHost();
+        }
+
+        public async Task Speak(string text, CancellationToken cancellationToken = new())
+        {
+            await Task.Run(async () =>
+            {
+                try
+                {
+                    var state = _talker.Speak(text);
+
+                    // 適当に結果をポーリングで監視する
+                    while (!state.IsCompleted) await Task.Delay(50, cancellationToken);
+                }
+                catch (OperationCanceledException)
+                {
+                    var result = _talker.Stop();
+                    Console.WriteLine(result);
+                    throw;
+                }
+            }, cancellationToken);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using CeVIOAIErrorTest;
+
+using var rikka = new KoharuRikka();
+
+while (true)
+{
+    var cts = new CancellationTokenSource();
+    _ = rikka.Speak("こんにちは、私は小春六花。", cts.Token);
+    await Task.Delay(500);
+    cts.Cancel();
+}
